1304837be17e59613a8d9467097a84aa0a958e8f
Mike <83963270+HypedMike@users.noreply.github.com>
2021-06-23 13:47:29 +0200

    added c++ math

--- javascript/animations.js
+++ javascript/animations.js
@@ -1,3 +1,7 @@
-function move_to(tag){
-    document.getElementById(tag).scrollIntoView()
+function move_to(tag1, tag2){
+    $(tag1).click(function() {
+        $([document.documentElement, document.body]).animate({
+            scrollTop: $(tag2).offset().top
+        }, 2000);
+    });
 }
