93d98415a9030eafe24b4c037b33ae02d4beddb2
Mike <83963270+HypedMike@users.noreply.github.com>
2025-06-07 14:36:55 +0200

    fix: add meta tags for improved SEO and accessibility

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,15 @@
     {@render children()}
 </main>
 
+<svelte:head>
+    <title> MS </title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8">
+    <meta name="description" content="A personal portfolio showcasing my projects and skills.">
+    <meta name="keywords" content="portfolio, projects, skills, web development, software engineering">
+    <meta name="author" content="Michele Saladino">
+</svelte:head>
+
 <script lang="ts">
 	import Navbar from "./navbar.svelte";
 
